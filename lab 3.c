2cde81f6c8537f97ab3b77fb6526bbb03a8016c4
jes64-ops <jessesedzro8@gmail.com>
2025-11-09 00:40:14 +0000

    rr

--- lab 3.c
+++ lab 3.c
@@ -1,40 +1,36 @@
-#include <stdio.h> 
-#include <stdbool.h>
-#define max_songs 7 //max number of songs is 7
+#include <stdio.h>// standard libary from input and output functions
+#include <stdlib.h>// for exit function
 
-struct songs_catalogue{  
-    char artist[50];
-    char song_name[50];
-    float price;
-    int year    ;
-} song[max_songs];
+#define max_songs 6 //max number of songs is 6 stored in the array
 
-int main(){
-    int choice;
-    printf("The following options are Avaliable:\n");
-    printf("Read in data[1]");
-    printf("Print Out the catalogue[2]");
-    printf("Save data to file[3]");
-    printf("Exit Program[4]");
-    printf("Enter choice Now:");
-    scanf("%d",choice);
-}
+struct songs_catalogue{  //defined a structure to store song information
+    char artist[50]; // array enables to store artists up to 50 characters 
+    char title[50];// enables array to store title of upto 50 characters 
+    float price; // float to store in array the prices
+    int year;// integer to store years in array 
+}song[max_songs]; //array of songs structres with max number of songs
 
-
-void read_in_data(){
-    int n=5, i; // set at least five songs integer
-    for (i=0; i<n; i++){
-        printf("Enter artist name:");
+void read_in_data() {// function to read songs from the standard keyboard input 
+    int i ,n =5;  //i = the loop counter
+    getchar();
+    for (i = 0; i <n ; i++) {// a loop is enabled to read each data from keyboad for each song 
+        
+        printf("Enter artist name:\n");//display asks for artist name
         fgets(song[i].artist, 50, stdin);
-        printf("Enter song name:");
-        fgets(song[i].song_name, 50, stdin);
-        printf("Enter price:");
-        scanf("%.2f", &song[i].price);
-        printf("Enter year:");
+
+        printf("Enter song title:\n");// display asks for song title 
+        fgets(song[i].title, 50, stdin);//reads input from standard keyboard and stores into title in each array
+
+        printf("Enter price:\n");//
+        scanf("%f", &song[i].price);
+
+        printf("Enter year:\n");
         scanf("%d", &song[i].year);
+        getchar();
     }
-    return 0;
+    main();
 }
+
 void save_data(){
     int n = 5, i;
     FILE *myPtr;
@@ -47,21 +43,80 @@ void save_data(){
         printf("The file is now opened.\n");
 
         for (i = 0; i < n; i++) {
-            fprintf(myPtr, "Artist: %s", song[i].artist);
-            fprintf(myPtr, "Song name: %s", song[i].song_name);
-            fprintf(myPtr, "Price: %.2f", song[i].price);
-            fprintf(myPtr, "Year: %d", song[i].year);
+            fprintf(myPtr, "%s %s %f %d", song[i].artist, song[i].title, song[i].price, song[i].year);
         }
-
         fclose(myPtr);
         printf("Songs have been saved to 'Song Catalogue.txt.\n");
     }
+    main();
 }
 
-void print_out_data(){
-    int n=5;
+void print_out_catologue_to_screen(){
     FILE *myPtr = fopen("Song_Catalogue.txt", "r");
     if (myPtr == NULL) {
-        printf("Error cannot not open file for reading.\n");
+        printf("Error: cannot open file for reading.\n");
         return;
-    while (fscanf(myPtr,))
+    }
+    int i, n= 5;
+    for(i=0;i <n; i++){
+        fgets(song[i].artist, 50, myPtr);
+        fgets(song[i].title,50,myPtr);
+        fscanf(myPtr,"%f",&song[i].price);
+        fscanf(myPtr,"%d",&song[i].year);
+        getchar();
+    }
+    fclose(myPtr);
+    sorting();
+    for (int j = 0; j < n; j++) {
+        printf("Artist:%s", song[j].artist);
+        printf("Title:%s", song[j].title);
+        printf("Price:%.2f\n", song[j].price);
+        printf("Year:%d\n", song[j].year);
+    }
+}
+
+
+void sorting() {// using bubble sorting
+    int n = 5, i; // number of songs
+    for (i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (song[j].year < song[j + 1].year) {//newsest to old check
+                // Swap entire struct
+                struct songs_catalogue temp = song[j];
+                song[j] = song[j + 1];
+                song[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    int choice;
+    printf("The following options are available:\n");
+    printf("Read in data[1]\n");
+    printf("Print Out the catalogue[2]\n");
+    printf("Save data to file[3]\n");
+    printf("Exit Program[4]\n");
+    printf("Enter choice Now:");
+    scanf("%d",&choice);
+
+    switch(choice){
+        case 1:
+        read_in_data();
+        break;
+        case 2:
+        print_out_catologue_to_screen();
+        break; 
+        case 3:
+        save_data();
+        break;
+        case 4:
+        exit(0);
+        printf("exiting program\n");
+        break;
+        default:
+        printf("INVALID CHOICE TRY AGAIN");
+    }
+}
